d8488b6849f3d843dc2481567beb8a7b7d840eae
radadevitasari <radadevitasari87.com>
2026-02-24 12:14:19 +0700

    dosen19

---
+++ Dosen19.java
@@ -0,0 +1,22 @@
+public class Dosen19 {
+
+    String kode;
+    String nama;
+    Boolean jenisKelamin; 
+    int usia;
+
+    public Dosen19(String kode, String nama, Boolean jenisKelamin, int usia) {
+        this.kode = kode;
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+        this.usia = usia;
+    }
+    void tampilInformasi() {
+        System.out.println("Kode           : " + kode);
+        System.out.println("Nama           : " + nama);
+        System.out.println("Jenis Kelamin  : " + (jenisKelamin ? "Laki-laki" : "Perempuan"));
+        System.out.println("Usia           : " + usia);
+        System.out.println("-------------------------------");
+    }
+}
+
