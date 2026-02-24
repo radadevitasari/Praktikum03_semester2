0bb4289274ebfbc4a4c41b712c5d227c01bfa7e5
radadevitasari <radadevitasari87.com>
2026-02-24 11:48:18 +0700

    tambahan method no 2 dan 3 pertanyaan 3.4.3

--- MataKuliah19.java
+++ MataKuliah19.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class MataKuliah19 {
 
     public String kode;
@@ -11,4 +13,26 @@ public class MataKuliah19 {
         this.sks = sks;
         this.jumlahJam = jumlahJam;
     }
+    public void tambahData(Scanner sc) {
+        String dummy;
+        System.out.print("Kode       : ");
+        kode = sc.nextLine();
+        System.out.print("Nama       : ");
+        nama = sc.nextLine();
+        System.out.print("SKS        : ");
+        dummy = sc.nextLine();
+        sks = Integer.parseInt(dummy);
+        System.out.print("Jumlah Jam : ");
+        dummy = sc.nextLine();
+        jumlahJam = Integer.parseInt(dummy);
+    }
+
+    public void cetakInfo() {
+        System.out.println("Kode       : " + kode);
+        System.out.println("Nama       : " + nama);
+        System.out.println("SKS        : " + sks);
+        System.out.println("Jumlah Jam : " + jumlahJam);
+        System.out.println("-----------------------------");
+    }
 }
+
--- MataKuliahDemo19.java
+++ MataKuliahDemo19.java
@@ -23,6 +23,14 @@ public class MataKuliahDemo19 {
             System.out.println("-----------------------------");
             arrayOfMataKuliah[i] = new MataKuliah19(kode, nama, sks, jumlahJam);
         }
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            System.out.println("Kode       : " + arrayOfMataKuliah[i].kode);
+            System.out.println("Nama       : " + arrayOfMataKuliah[i].nama);
+            System.out.println("SKS        : " + arrayOfMataKuliah[i].sks);
+            System.out.println("Jumlah Jam : " + arrayOfMataKuliah[i].jumlahJam);
+            System.out.println("-----------------------------");
+        }
     }
 }
 
